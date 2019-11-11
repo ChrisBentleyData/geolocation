098ffe5811b1c4b244926dd2c4f6595122367b89
ChrisBentleyData <57326993+ChrisBentleyData@users.noreply.github.com>
2019-11-10 23:59:29 -0500

    Updated processor to connect to DB and retrieve state

--- myprocessor.py
+++ myprocessor.py
@@ -1,4 +1,30 @@
-class MyProcessor:     
-    def run(self, df):        
-        return df.agg(['mean', 'min', 'max'])
-
+class MyProcessor:
+    def run(self, addr):   
+        import pandas as pd
+        import googlemaps
+        import psycopg2
+        
+        google_api_key = ''
+        dbname = 'GISDemo'
+        user = ''
+        password = ''
+        shape_table = 'public.cb_2018_us_state500k2'
+        
+        gmaps = googlemaps.Client(key=google_api_key)
+        
+        # Geocoding the address
+        geocode_result = gmaps.geocode(addr)
+        
+        lat = str(geocode_result[0]['geometry']['location']['lat'])
+        lng = str(geocode_result[0]['geometry']['location']['lng'])
+        
+        conn_str = 'dbname=' + dbname + ' user=' + user + ' password=' + password
+        conn = psycopg2.connect(conn_str)
+        
+        cur = conn.cursor()
+        qry = "Select name FROM " + shape_table + " where st_intersects(geom, ST_SetSrid(ST_GeomFromText('POINT(" + lng + " " + lat + ")'),4269))"
+        cur.execute(qry)
+        state = cur.fetchone()[0]
+        cur.close()
+        
+        return pd.DataFrame({'State':[state]})
